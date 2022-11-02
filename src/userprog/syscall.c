57cfd7141a6e216eceaf5f375e807a2e6c29aa6b
CannanWong <wongcannan@gmail.com>
2022-11-02 15:56:20 +0000

    Function handling undefined accesses for user-provided pointers added

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -3,6 +3,9 @@
 #include <syscall-nr.h>
 #include "threads/interrupt.h"
 #include "threads/thread.h"
+#include "threads/vaddr.h"
+#include "pagedir.h"
+#include "process.h"
 
 static void syscall_handler (struct intr_frame *);
 
@@ -10,6 +13,17 @@ void
 syscall_init (void) 
 {
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+}
+
+/*  Function handling checks to user-provided pointers.
+    Calls functions to clear allocated memory to the current process
+    and kills the current thread. */
+void
+valid_pointer (uint32_t *pd, const void *uaddr) {
+  if (pagedir_get_page(pd, uaddr) == NULL || !is_user_vaddr(uaddr)) {
+    process_exit();
+    thread_exit();
+  }
 }
 
 static void
