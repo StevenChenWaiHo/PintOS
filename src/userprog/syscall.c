ac50009ce075f8b0157f6e01a49e342358f0bcdc
CannanWong <wongcannan@gmail.com>
2022-11-04 16:15:33 +0000

    Draft for syscall_handler, not tested

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -12,23 +12,66 @@ static void syscall_handler (struct intr_frame *);
 void
 syscall_init (void) 
 {
+  //TODO: Implement stack initialization
   intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 /*  Function handling checks to user-provided pointers.
     Calls functions to clear allocated memory to the current process
     and kills the current thread. */
-void
-valid_pointer (uint32_t *pd, const void *uaddr) {
-  if (pagedir_get_page(pd, uaddr) == NULL || !is_user_vaddr(uaddr)) {
-    process_exit();
-    thread_exit();
+static void
+valid_pointer (const void *uaddr) {
+  //? Check for 3 arg fields?
+  if (!is_user_vaddr()
+    || !pagedir_get_page(thread_current()->pagedir, uaddr)) {
+    exit_handler ();
   }
 }
 
 static void
-syscall_handler (struct intr_frame *f UNUSED) 
+exit_handler () {
+  //? Only these two functions?
+  process_exit ();
+  thread_exit ();
+}
+
+static void
+syscall_handler (struct intr_frame *f) 
 {
-  printf ("system call!\n");
+  valid_pointer(f->esp);
+  int args[3] = {0}; //?
+  void *p = f->esp;
+  int sys_call_num = &p;
+  switch (sys_call_num) {
+    case SYS_HALT: 
+    {
+      sys_call[SYS_HALT]();
+    }
+    case SYS_EXIT: case SYS_EXEC: case SYS_WAIT:
+    case SYS_REMOVE: case SYS_OPEN: case SYS_FILESIZE:
+    case SYS_TELL: case SYS_CLOSE:
+    {
+      valid_pointer(p++);
+      args[0] = &p;
+      sys_call[sys_call_num](args[0]);
+    }
+    case SYS_CREATE: case SYS_SEEK:
+    {
+      valid_pointer(p++);
+      args[1] = &p;
+      sys_call[sys_call_num](args[0], args[1]);
+    }
+    case SYS_READ: case SYS_WRITE:
+    {
+      valid_pointer(p++);
+      args[2] = &p;
+      sys_call[sys_call_num](args[0], args[1], args[2]);
+    }
+    default:
+    {
+      exit_handler ();
+    }
+  }
+  printf("Call complete.");
   thread_exit ();
 }
--- src/userprog/syscall.h
+++ src/userprog/syscall.h
@@ -1,6 +1,35 @@
 #ifndef USERPROG_SYSCALL_H
 #define USERPROG_SYSCALL_H
 
+#include <debug.h>
+#include <stdbool.h>
+
+#define PTSIZE 4
+
+typedef int pid_t;
+
 void syscall_init (void);
+
+/* System call function prototypes. */
+void halt (void) NO_RETURN;
+void exit (int status) NO_RETURN;
+pid_t exec (const char *file);
+int wait (pid_t);
+bool create (const char *file, unsigned initial_size);
+bool remove (const char *file);
+int open (const char *file);
+int filesize (int fd);
+int read (int fd, void *buffer, unsigned length);
+int write (int fd, const void *buffer, unsigned length);
+void seek (int fd, unsigned position);
+unsigned tell (int fd);
+void close (int fd);
+
+void (*sys_call[13])() = {
+    halt, exit, exec, wait,
+    create, remove, open, filesize,
+    read, write, seek, tell, close
+};
+
 
 #endif /* userprog/syscall.h */
